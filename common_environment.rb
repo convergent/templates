7c120a6983ef785c10342e30e60508e1d61e41f4
Michelangelo Altamore <michelangelo@altamore.org>
2009-03-29 12:44:52 +0200

    common gems and plugins go here

---
+++ common_environment.rb
@@ -0,0 +1,8 @@
+gem 'haml',
+gem 'giraffesoft-resource_controller', :lib => 'resource_controller', :source => 'http://gems.github.com'
+gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+
+gem 'giraffesoft-enum_field', :lib => 'enum_field', :source => 'http://gems.github.com'
+plugin 'default_value_for', :git => 'git://github.com/FooBarWidget/default_value_for.git', :submodule => true
+
+# paperclip
